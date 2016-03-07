466adedec268a680fe8c95921075c1ecf1e5fda5
hari <harishpaulrocks@gmail.com>
2016-03-07 23:53:30 +0530

    3

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.sunshine.app;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -9,8 +10,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -23,40 +26,85 @@ import java.util.Arrays;
 import java.util.List;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new ForecastFragment())
-                    .commit();
-        }
+        setContentView(R.layout.activity_2);
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction()
+//                    .add(R.id.container, new ForecastFragment())
+//                    .commit();
+     //   }
+    }
+    public int numberOfPoints1 = 0;
+    public int numberOfPoints2 = 0;
+    public void add21(View view){
+        numberOfPoints1 += 2;
+
+        displayScore1(numberOfPoints1);
+    }
+    public void add11(View view){
+        numberOfPoints1 += 1;
+        displayScore1(numberOfPoints1);
+    }
+    public void minus11(View v) {
+        numberOfPoints1 -= 1;
+        displayScore1(numberOfPoints1);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+
+    public void displayScore1(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.score_text_view1);
+        quantityTextView.setText("" + number);
+    }
+    public void add22(View view){
+        numberOfPoints2 += 2;
+
+        displayScore2(numberOfPoints2);
+    }
+    public void add12(View view){
+        numberOfPoints2 += 1;
+        displayScore2(numberOfPoints2);
+    }
+    public void minus12(View v) {
+        numberOfPoints2 -= 1;
+        displayScore2(numberOfPoints2);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void displayScore2(int number){
+        TextView quantityTextView = (TextView) findViewById(R.id.score_text_view2);
+        quantityTextView.setText("" + number);
     }
+    public void reset(View v) {
+        displayScore1(0);
+        displayScore2(0);
+        numberOfPoints1 = 0;
+        numberOfPoints2 = 0;
+    }
+    //@Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+
+    //@Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
 
     /**
